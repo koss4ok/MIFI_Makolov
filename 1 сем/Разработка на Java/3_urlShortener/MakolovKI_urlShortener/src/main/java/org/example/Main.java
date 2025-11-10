985255a7c09b742dadab4171737f2eeb654cef92
Константин <koss4ok@gmail.com>
2025-11-10 19:14:41 +0500

    MVP Итоговый проект Программирование на java

--- 1 сем/Разработка на Java/3_urlShortener/MakolovKI_urlShortener/src/main/java/org/example/Main.java
+++ 1 сем/Разработка на Java/3_urlShortener/MakolovKI_urlShortener/src/main/java/org/example/Main.java
@@ -1,17 +1,82 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.awt.*;
+import java.nio.file.Path;
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+    protected static Path csvPath = Path.of("./DB.csv");
+    protected static String domain = "http://Makolov.ru/";
+    protected static Scanner scanner;
+    protected static UUID currentUserId;
+    public static void main(String[] args) throws Exception {
+        scanner = new Scanner(System.in);
+        auth();
+        while (true){
+            System.out.println("Введите команду. Список доступных комманд можно увидеть написав ?");
+            var command = scanner.nextLine().strip();
+            if (command.startsWith("http")) {
+                if(isShort(command)){
+                    var curUrl = UrlInfo.TryGetUrl(currentUserId, command);
+                    if (curUrl != null){
+                        curUrl.Open();
+                    }
+                    else
+                        System.out.println("Ссылка не найдена");
+                }
+                else{
+                    System.out.println("Сколько дней должна быть активна ссылка?");
+                    var daysOfActive = Integer.parseInt(scanner.nextLine().strip());
+                    System.out.println("Сколько переходов по ссылке можно осуществить?");
+                    var limit = Integer.parseInt(scanner.nextLine().strip());
+                    var newUrl = new UrlInfo(command, currentUserId, daysOfActive, limit);
+                    System.out.println("Короткая ссылка создана: " + newUrl.ShortUrl);
+                }
+            }
+            if(command.equals("?")) {
+                System.out.println(
+                        """
+                        Список доступных комманд:
+                        1) Exit - выход и завершение работы.
+                        2) ? - список доступных команд.
+                        3) Switch - смена текущего пользователя
+                        3) Ввод полной ссылки - автоматически будет сформированна короткая ссылка.
+                        4) Ввод короткой ссылки - будет выполнен редирект и открыта страница из полной ссылки.""");
+            }
+            if(command.equals("Switch"))
+                Switch();
+            if(command.equals("Exit")){
+                return;
+            }
         }
     }
+    private static void Switch(){
+        while (true) {
+            System.out.println("Вы хотите сменить пользователя? (y/n)");
+            if(scanner.nextLine().strip().equals("y")){
+                auth();
+                return;
+            }
+            if(scanner.nextLine().strip().equals("n"))
+                return;
+        }
+    }
+    private static void auth(){
+        System.out.println("Введите свой Id или просто нажмите Enter для нового пользователя");
+        var answer = scanner.nextLine().trim();
+        if(answer.isBlank()){
+            var id = UUID.randomUUID();
+            System.out.println("Ваш Id = " + id);
+            currentUserId =  id;
+        }
+        else
+            currentUserId = UUID.fromString(answer);
+    }
+
+    private static boolean isShort(String url){
+        return url.startsWith(domain);
+    }
+
+
 }
---
+++ 1 сем/Разработка на Java/3_urlShortener/MakolovKI_urlShortener/src/main/java/org/example/UrlInfo.java
@@ -0,0 +1,103 @@
+package org.example;
+
+import java.awt.*;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.security.MessageDigest;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HexFormat;
+import java.util.UUID;
+
+import static org.example.Main.csvPath;
+import static org.example.Main.domain;
+
+public class UrlInfo
+{
+    public String FullUrl;
+    public String ShortUrl;
+    public UUID AuthorId;
+    public LocalDateTime ValidUntil;
+    public int Limit;
+    public int LinkOpens;
+
+    private UrlInfo(String line){
+        var data = line.split(";");
+        this.FullUrl = data[0];
+        this.ShortUrl = data[1];
+        this.AuthorId = UUID.fromString(data[2]);
+        this.ValidUntil = LocalDateTime.parse(data[3]);
+        this.Limit = Integer.parseInt(data[4]);
+        this.LinkOpens = Integer.parseInt(data[5]);
+
+    }
+    public UrlInfo(String fullUrl, UUID authorId, int daysOfActive , int limit ) throws Exception{
+        this.FullUrl = fullUrl;
+        this.ValidUntil = LocalDateTime.now().plusDays(daysOfActive);
+        this.Limit = limit;
+        this.AuthorId = authorId;
+        this.LinkOpens = 0;
+        this.ShortUrl = createShortUrl(fullUrl, authorId);
+        Files.writeString(csvPath, this.fieldsToString(),  StandardOpenOption.APPEND);
+    }
+    public static UrlInfo TryGetUrl (UUID authorId, String shortUrl) throws Exception {
+        var temp = GetAllInfo().stream().filter(item-> item.ShortUrl.equals(shortUrl) && item.AuthorId.equals(authorId)).findFirst().orElse(null);
+        if(temp == null)
+            return null;
+        if(temp.Limit <= temp.LinkOpens){
+            System.out.println("У ссылки закончились переходы");
+            DeleteLinkData(temp);
+            return null;
+        }
+        if(temp.ValidUntil.isBefore(LocalDateTime.now())){
+            System.out.println("Срок жизни ссылки закончился");
+            DeleteLinkData(temp);
+            return null;
+        }
+
+        return temp;
+    }
+    private static void DeleteLinkData(UrlInfo url) throws Exception {
+        var list = GetAllInfo();
+        list.removeIf(item-> item.fieldsToString().equals(url.fieldsToString()));
+        Files.writeString(csvPath, "");
+        for(var item :list){
+            Files.writeString(csvPath, item.fieldsToString(), StandardOpenOption.APPEND);
+        }
+    }
+    private static void UpdateCounter(UrlInfo url) throws Exception {
+        var list = GetAllInfo();
+        list.removeIf(item-> item.fieldsToString().equals(url.fieldsToString()));
+        url.LinkOpens++;
+        list.add(url);
+        Files.writeString(csvPath, "");
+        for(var item :list){
+            Files.writeString(csvPath, item.fieldsToString(), StandardOpenOption.APPEND);
+        }
+    }
+    public void Open() throws Exception {
+        UpdateCounter(this);
+        Desktop.getDesktop().browse(new URI(this.FullUrl));
+    }
+
+    private String fieldsToString() {
+        return this.FullUrl + ";" + this.ShortUrl + ";" + this.AuthorId + ";" + this.ValidUntil + ";" + this.Limit + ";" + this.LinkOpens+"\r\n";
+    }
+    public static ArrayList<UrlInfo> GetAllInfo() throws Exception {
+        var list = new ArrayList<UrlInfo>();
+        for(var line: Files.readAllLines(csvPath)){
+            var item = new UrlInfo(line);
+            list.add(item);
+        }
+        return list;
+    }
+
+    private static String createShortUrl(String fullUrl, UUID userId) throws Exception{
+        var stringToHash = userId.toString()+fullUrl;
+        var digest = MessageDigest.getInstance("SHA-256");
+        var hashBytes = digest.digest(stringToHash.getBytes());
+        var hash = HexFormat.of().formatHex(hashBytes);
+        return domain + hash.substring(0,10);
+    }
+}
